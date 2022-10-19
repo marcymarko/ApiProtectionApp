95a3d35cf13faf2f6d1ecf9f95f8a3a15b3f5992
Marko Zilkovskyi <marko.zilkovskyi@msc.com>
2022-10-19 10:52:00 -0400

    Add project files.

---
+++ ApiProtection/Program.cs
@@ -0,0 +1,33 @@
+using AspNetCoreRateLimit;
+using ApiProtection.StartupConfig;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddResponseCaching();
+
+builder.Services.AddMemoryCache();
+builder.AddRateLimitServices();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseResponseCaching();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseIpRateLimiting();
+
+app.Run();
